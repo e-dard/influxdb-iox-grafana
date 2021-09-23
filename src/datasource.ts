00c4d4497caf71c7e670007f546a08040274389f
Edd Robinson <me@edd.io>
2021-09-23 22:52:06 +0100

    feat: template replacement

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,9 +1,17 @@
 import { DataSourceInstanceSettings } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
 import { MyDataSourceOptions, MyQuery } from './types';
+import { getTemplateSrv } from '@grafana/runtime';
 
 export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+  }
+  applyTemplateVariables(query: MyQuery) {
+    const templateSrv = getTemplateSrv();
+    return {
+      ...query,
+      queryText: query.queryText ? templateSrv.replace(query.queryText) : '',
+    };
   }
 }
